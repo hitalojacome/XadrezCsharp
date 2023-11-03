b2cfe8a5b2bd725871101f49f630b5ff71135506
Hitalo Jacome <jacomehitalo@gmail.com>
2023-11-03 11:27:20 -0300

    Optimizing board display in the console

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -6,11 +6,6 @@ namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
 
-            ChessPosition pos = new ChessPosition('C', 7);
-            Console.WriteLine(pos);
-
-            Console.WriteLine(pos.toPosition());
-
             try 
             {
                 Chessboard board = new Chessboard(8,8);
@@ -18,12 +13,16 @@ namespace xadrez_console {
                 board.PlacePiece(new Rook(board, Color.Black), new Position(1,3));
                 board.PlacePiece(new King(board, Color.Black), new Position(0,2));
 
+                board.PlacePiece(new Rook(board, Color.White), new Position(3,5));
+                board.PlacePiece(new Rook(board, Color.White), new Position(7,3));
+                board.PlacePiece(new King(board, Color.White), new Position(7,0));
+
                 Screen.printChessboard(board);
             }
             catch (ChessException e)
             {
                 Console.WriteLine(e.Message);
-            }
+            }            
         }
     }
 }
--- xadrez_console/Screen.cs
+++ xadrez_console/Screen.cs
@@ -9,16 +9,34 @@ namespace xadrez_console
         {
 
             for(int i=0; i<board.lines; i++) {
+                Console.Write($"{8-i}  ");
                 for(int j=0; j<board.columns; j++) {
                     if(board.Piece(i,j) == null) {
                         Console.Write("- "); // Imprime um hífen para espaços vaziox
                     } else { 
-                        Console.Write($"{board.Piece(i,j)} "); // Imprime a representação da peça 
+                        Screen.printPiece(board.Piece(i, j)); // Imprime a representação da peça 
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("   A B C D E F G H");
 
+        }
+
+        public static void printPiece(Piece piece)
+        {
+            if (piece.color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+
+            }
         }
     }
 }
