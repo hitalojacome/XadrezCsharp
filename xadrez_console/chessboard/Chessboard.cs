30ccc864d63d717f8bb642ab6947595e830d6cf4
Hitalo Jacome <jacomehitalo@gmail.com>
2023-11-03 09:20:54 -0300

    Creating Chessboard class

--- xadrez_console/chessboard/Chessboard.cs
+++ xadrez_console/chessboard/Chessboard.cs
@@ -2,6 +2,14 @@ namespace chessboard
 {
     public class Chessboard
     {
-        
+        public int lines { get; set; }
+        public int columns { get; set; }
+        private Piece[,] pieces;
+
+        public Chessboard(int lines, int columns) {
+            this.lines = lines;
+            this.columns = columns;
+            pieces = new Piece[lines, columns];
+        }
     }
 }
