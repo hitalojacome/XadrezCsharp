11b6e80a469d64bf4ca378a1f44c9bd68e585f88
Hitalo Jacome <jacomehitalo@gmail.com>
2023-11-06 13:30:14 -0300

    exception handling

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -5,14 +5,19 @@ using chessboard;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
+            try {
+                Chessboard board = new(8,8);
 
-            Chessboard board = new(8,8);
+                board.InsertPiece(new Rook(board, Color.Black), new Position(0,0));
+                board.InsertPiece(new Rook(board, Color.Black), new Position(1,3));
+                board.InsertPiece(new King(board, Color.Black), new Position(0,9));
 
-            board.InsertPiece(Rook, new Position(0,0));
-            board.InsertPiece(Rook, new Position(1,3));
-            board.InsertPiece(King, new Position(2,4));
-
-            Screen.PrintChessboard(board);
+                Screen.PrintChessboard(board);
+            }
+            catch(ChessException e) // Tratamento de erros
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- xadrez_console/chessboard/Chessboard.cs
+++ xadrez_console/chessboard/Chessboard.cs
@@ -24,13 +24,56 @@ namespace chessboard
             return _pieces[line, column];
         }
 
+        // Sobrecarga da classe Piece
+        public Piece Piece(Position position) 
+        {
+            return _pieces[position.Line, position.Column];
+        }
+
+        // Método que verifica se já existe uma peça na posição passada
+        public bool PieceExist(Position position)
+        {
+            // Primeiro verifica se a posição é valida
+            PositionValidation(position);
+            // Retorna a peça na posição se não for nulo
+            return Piece(position) != null;
+        }
+
         // Método para inserção de peça no tabuleiro
         public void InsertPiece(Piece piece, Position position)
         {
+            // Antes da inserção de peça valida se existe peça no local indicado
+            if (PieceExist(position)) // Como na classe PieceExist já está incluido a validação da class PositionValidation, acaba validando ambos antes de inserir uma peça
+            {
+                throw new ChessException("Already exist a piece in this position!");
+            }
             // A matriz é uma peça
             _pieces[position.Line, position.Column] = piece;
             // A posição da peça é a posição passada
             piece.Position = position;
+        }
+
+        // Método para validar se a posição é válida
+        public bool ValidPosition(Position position)
+        {
+            /* SE o valor de linha for menor que 0 ou maior que a quantidade em linhas
+            OU SE o valor de coluna for menor que 0 ou maior que a quantidade em colunas */
+            // linha e coluna = atributos Position || linhas e colunas = atributos Chessboard
+            if (position.Line<0 || position.Line>=Lines || position.Column<0 || position.Column>=Columns) 
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // Método para lançar a exceção caso a posição seja inválida
+        public void PositionValidation(Position position)
+        {
+            // SE a posição passada em ValidPosition NÃO(!) for válida
+            if (!ValidPosition(position))
+            {
+                throw new ChessException("Invalid position!");
+            }
         }
     }
 }
