c2ff2ad9a0d808ef63f48b8cd919d1dc28b30b68
Hitalo Jacome <jacomehitalo@gmail.com>
2023-11-17 13:51:20 -0300

    Comentando outras peças

--- xadrez_console/chess/ChessMatch.cs
+++ xadrez_console/chess/ChessMatch.cs
@@ -79,20 +79,20 @@ namespace chess
         private void SetupPieces()
         {
             // Peças brancas
-            Board.InsertPiece(new Rook(Board, Color.White), new ChessPosition('c', 1).ToPosition());
+            //Board.InsertPiece(new Rook(Board, Color.White), new ChessPosition('c', 1).ToPosition());
             Board.InsertPiece(new Rook(Board, Color.White), new ChessPosition('c', 2).ToPosition());
-            Board.InsertPiece(new Rook(Board, Color.White), new ChessPosition('d', 2).ToPosition());
-            Board.InsertPiece(new Rook(Board, Color.White), new ChessPosition('e', 2).ToPosition());
-            Board.InsertPiece(new Rook(Board, Color.White), new ChessPosition('e', 1).ToPosition());
-            Board.InsertPiece(new King(Board, Color.White), new ChessPosition('d', 1).ToPosition());
+            //Board.InsertPiece(new Rook(Board, Color.White), new ChessPosition('d', 2).ToPosition());
+            //Board.InsertPiece(new Rook(Board, Color.White), new ChessPosition('e', 2).ToPosition());
+            //Board.InsertPiece(new Rook(Board, Color.White), new ChessPosition('e', 1).ToPosition());
+            //Board.InsertPiece(new King(Board, Color.White), new ChessPosition('d', 1).ToPosition());
 
             // Peças pretas
-            Board.InsertPiece(new Rook(Board, Color.Black), new ChessPosition('c', 7).ToPosition());
-            Board.InsertPiece(new Rook(Board, Color.Black), new ChessPosition('c', 8).ToPosition());
-            Board.InsertPiece(new Rook(Board, Color.Black), new ChessPosition('d', 7).ToPosition());
-            Board.InsertPiece(new Rook(Board, Color.Black), new ChessPosition('e', 7).ToPosition());
-            Board.InsertPiece(new Rook(Board, Color.Black), new ChessPosition('e', 8).ToPosition());
-            Board.InsertPiece(new King(Board, Color.Black), new ChessPosition('d', 8).ToPosition());
+            //Board.InsertPiece(new Rook(Board, Color.Black), new ChessPosition('c', 7).ToPosition());
+            //Board.InsertPiece(new Rook(Board, Color.Black), new ChessPosition('c', 8).ToPosition());
+            //Board.InsertPiece(new Rook(Board, Color.Black), new ChessPosition('d', 7).ToPosition());
+            //Board.InsertPiece(new Rook(Board, Color.Black), new ChessPosition('e', 7).ToPosition());
+            //Board.InsertPiece(new Rook(Board, Color.Black), new ChessPosition('e', 8).ToPosition());
+            //Board.InsertPiece(new King(Board, Color.Black), new ChessPosition('d', 8).ToPosition());
         }
     }
 }
